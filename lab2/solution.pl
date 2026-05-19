728a1b245aa1aa5fe2c66d02f76f2b5d8a83c719
knst-l <a1z9valeria@gmail.com>
2026-05-20 00:16:30 +0800

    Add merge sort relation to lab 2

--- lab2/solution.pl
+++ lab2/solution.pl
@@ -6,3 +6,16 @@ merge([X | T1], [Y | T2], [X | T]) :-
 merge([X | T1], [Y | T2], [Y | T]) :-
     X > Y,
     merge([X | T1], T2, T).
+
+split([], [], []).
+split([X], [X], []).
+split([X, Y | T], [X | T1], [Y | T2]) :-
+    split(T, T1, T2).
+
+merge_sort([], []).
+merge_sort([X], [X]).
+merge_sort(L, S) :-
+    split(L, L1, L2),
+    merge_sort(L1, S1),
+    merge_sort(L2, S2),
+    merge(S1, S2, S).
